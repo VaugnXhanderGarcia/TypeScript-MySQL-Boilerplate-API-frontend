0cf79bb539f551222696aae770ac954c08f0c306
Vaugn Xhander A. Garcia <xhander.garcia@gmail.com>
2026-05-12 03:47:42 +0800

    Fix refresh token session restore

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -1,12 +1,10 @@
-import { catchError, of } from 'rxjs';
 import { AccountService } from '../_services';
 
 export function appInitializer(accountService: AccountService) {
   return () =>
     new Promise<void>((resolve) => {
-      accountService.refreshToken().subscribe({
-        next: () => resolve(),
-        error: () => resolve()
-      });
+      accountService.refreshToken()
+        .subscribe()
+        .add(resolve);
     });
 }
