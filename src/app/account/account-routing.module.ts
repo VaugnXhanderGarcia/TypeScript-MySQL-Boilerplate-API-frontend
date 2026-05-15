cc27f79c18bd80ba318637ab8c6e1fbcd6bc234d
Vaugn Xhander A. Garcia <xhander.garcia@gmail.com>
2026-05-15 17:15:34 +0800

    Fix production API URL and auth session behavior

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+
 import { LayoutComponent } from './layout.component';
 import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register.component';
