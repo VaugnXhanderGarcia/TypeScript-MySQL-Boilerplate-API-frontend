de77a2fe4422af859a7fda65615f3202006ba304
Vaugn Xhander A. Garcia <xhander.garcia@gmail.com>
2026-05-18 17:51:38 +0800

    fix: Update environment configuration for production with new API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
-export const environment = {
+/*export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000'
+};*/
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://angular-auth-final.onrender.com'
 };
